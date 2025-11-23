9df2ba55b78b931df4376eb07fbc3ac45aeb5c7b
Alex Yesmanchyk <aliaksandr.yesmanchyk@gmail.com>
2025-11-23 18:54:19 -0500

    Added Hana struct generator

---
+++ hana.lua
@@ -0,0 +1,27 @@
+nstructs = 4
+nfields  = 8
+
+for i=0,nstructs do
+  struct_header = string.format("struct S%d {", i)
+  lines = {struct_header}
+  fields = {}
+  values = {}
+  kvs = {}
+  for j=0,nfields do
+    lines[#lines+1] = string.format("  int f%d;", j)
+    kvs[#kvs+1] = string.format("f%d: %d ", j, j+1)
+    fields[#fields+1] = string.format("f%d", j)
+    values[#values+1] = tostring(1+j)
+  end
+  lines[#lines+1] = "};"
+
+  lines[#lines+1] = string.format(
+    "BOOST_HANA_ADAPT_STRUCT(S%d, %s);",
+    i, table.concat(fields, ", ")
+  )
+  lines[#lines+1] = string.format(
+    "S%d s%d{%s};", i, i, table.concat(values, ",")
+  )
+  table.insert(lines, "")
+  vim.api.nvim_buf_set_lines(0, 3+i*#lines, 3+i*#lines, true, lines)
+end
